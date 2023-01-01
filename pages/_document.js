3776ed3db93ea07a0634e045adb6bb233cf613e3
Aditya Malani <aditya@Adityas-MacBook-Pro-3.local>
2023-01-01 23:38:07 +0530

    UI version 1 done

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
-        <meta
-          property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
-        />
+        <meta property="og:title" content="Free Online AI Email Writer" key="title"/>
+        <meta property="og:description" content="Get professional emails written by AI for free in just seconds with our Online AI Email Writer. 
+          Simply input your desired email content and our AI will generate a polished, professional email that is ready to send. 
+          Save time and effort while still maintaining a high level of quality with our AI email writing tool." key="description"/>
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import { useState } from 'react';
 
 const Home = () => {
 
-  const [userInput, setUserInput] = useState('');
+  const [userInputTo, setUserInputTo] = useState('');
+  const [userInputContent, setUserInputContent] = useState('');
   const [apiOutput, setApiOutput] = useState('')
   const [isGenerating, setIsGenerating] = useState(false)
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
-    console.log(userInput)
+    console.log(userInputContent)
     // const response = await fetch('/api/generate', {
     //   method: 'POST',
     //   headers: {
@@ -27,62 +28,111 @@ const Home = () => {
     setIsGenerating(false);
   }
 
-  const onUserChangedText = (event) => {
-    setUserInput(event.target.value);
+  const onUserChangedTextTo = (event) => {
+    setUserInputTo(event.target.value);
+  };
+
+  const onUserChangedTextContent = (event) => {
+    setUserInputContent(event.target.value);
   };
 
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Message Writer</title>
+        <title>Free Online AI Email Writer</title>
+        <meta name="description" content="Get professional emails written by AI for free in just seconds with our Online AI Email Writer. 
+          Simply input your desired email content and our AI will generate a polished, professional email that is ready to send. 
+          Save time and effort while still maintaining a high level of quality with our AI email writing tool."
+          key="desc"
+        />
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>AI Professional Message Writer</h1>
+            <h1>Online Email Writer</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Because, how you say it is more important than what you say!</h2>
+            <h2>Generate high quality emails using AI for free within seconds!</h2>
           </div>
-          <div className="header-subtitle">
-            <h2>Use the tool below to generate professional message for any scenario in seconds!</h2>
-          </div>
+        </div>
+      </div>
 
+      <div className="row">
+        <div className="column">
           <div className="prompt-container">
+            <p className='label'>
+              To:
+            </p>
             <textarea 
-            placeholder="start typing here" 
-            className="prompt-box" 
-            value={userInput}
-            onChange={onUserChangedText}/>
-            
+              placeholder="E.g. Manager, Sales Client, HR, etc." 
+              className="prompt-box-to" 
+              value={userInputTo}
+              onChange={onUserChangedTextTo}/>
+              
+            <p className='label'>
+              Email Content:
+            </p>
+            <textarea 
+              placeholder="E.g. I will be on leave from Jan 1 to Jan 10. I Will ensure progress on my ongoing project is not affected in my absence." 
+              className="prompt-box-content" 
+              value={userInputContent}
+              onChange={onUserChangedTextContent}/>
+              
             <div className="prompt-buttons">
               <a
-                className={isGenerating ? 'generate-button loading' : 'generate-button'}
-                onClick={callGenerateEndpoint}
-              >
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}>
                 <div className="generate">
-                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+                  {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
                 </div>
               </a>
             </div>
-
+          </div>
+        </div>
+        <div className='column'>
+          <div className='output-box'>
             {apiOutput && (
               <div className="output">
-                <div className="output-header-container">
-                  <div className="output-header">
-                    <h3>Output</h3>
-                  </div>
-                </div>
                 <div className="output-content">
                   <p>{apiOutput}</p>
                 </div>
               </div>
             )}
           </div>
-          </div>
-
         </div>
+    </div>
+    <div className='row'>
+      <div className="about-product">
+        <h2>How to use?</h2>
+        <p>Step 1: Enter whom you want send the email to. Be as detailed as possible while providing this. For eg - 
+          "senior memebr from an external team, client who has received a product demo from you earlier, etc."</p>
+        <p>Step 2: Enter briefly what message you want to convey. Use bullet points if required.</p>
+        <p>Step 3: Click on Generate</p>
       </div>
+    </div>
+    <div className='row'>
+      <div className="about-product">
+        <h2>What is Free Online AI Email Writer?</h2>
+        <p>
+        Effortlessly create professional, effective and grammatically correct emails with our free AI-powered online email writer. 
+        Simply provide a few brief points about the message you want to convey, and our artificial intelligence technology 
+        will generate a customized email with a professional subject line and engaging content. Our email writer is perfect for busy professionals 
+        looking to save time on writing emails or anyone looking to communicate more effectively. 
+        Some common use cases include:
+        </p>
+        <ul>
+          <li>Sending a follow-up email after a meeting</li>
+          <li>Requesting a favor or additional information</li>
+          <li>Responding to a customer inquiry or complaint</li>
+          <li>Making a sales pitch or introducing a new product or service</li>
+          <li>Creating a personalized email to a potential employer</li>
+          <li>Crafting a professional email to a client or colleague</li>
+          <li>And many more...</li>
+        </ul>
+        <p>Try our email writer now and see the difference it can make in your communication. Because how you say is more important than what you say!</p>
+      </div>
+    </div>
+    </div>
   );
 };
 
